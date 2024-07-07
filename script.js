33e0addc1941475512999b1685dd6889682a0688
Gururaj R Sankannanavar <95588353+Gururaj18@users.noreply.github.com>
2024-07-07 13:52:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('https://my-json-server.typicode.com/Gururaj18/socialmedia/users');
+        const response = await fetch('https://socialbackend-4uk5.onrender.com/users');
         const data = await response.json();
 
         const user = data.find(u => u.username === username && u.password === password);
